8d7d7c0ebf0959e1872f5d5e713a94aa07f1a4dc
HugoOliveiraSoares <hugoliveira.soares@gmail.com>
2020-01-08 14:26:56 -0300

    back

---
+++ mundo1:fundamentos/exer2.py
@@ -0,0 +1,4 @@
+dia = input('Digite o dia em que nasceu: ')
+mes = input('Digite o mes em que nasceu: ')
+ano = input('Digite o ano em que nasceu: ')
+print('Você nasceu no dia ' + dia + ' de ' + mes + ' de ' + ano + '. Correto?')
